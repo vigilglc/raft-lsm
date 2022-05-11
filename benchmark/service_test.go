2d6411158107adbba9f6a1cc3bba9b196aa60088
gaolc18 <gaolc18@fudan.edu.cn>
2022-05-12 01:29:19 +0800

    Add benchmark for etcd and raft-lsm service.

---
+++ benchmark/service_test.go
@@ -0,0 +1,123 @@
+package benchmark
+
+import (
+	"context"
+	"github.com/vigilglc/raft-lsm/client"
+	"github.com/vigilglc/raft-lsm/server/api/rpcpb"
+	"github.com/vigilglc/raft-lsm/server/backend"
+	"github.com/vigilglc/raft-lsm/server/backend/kvpb"
+	clientv3 "go.etcd.io/etcd/client/v3"
+	"testing"
+	"time"
+)
+
+func Benchmark_RaftLSM_Service_Put(b *testing.B) {
+	agt := client.NewAgent(context.Background(), "localhost:400", "localhost:401", "localhost:402")
+	defer func(agt client.Agent) { _ = agt.Close() }(agt)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		err := agt.Pick(func(ctx context.Context, c client.Client) error {
+			_, err := c.Put(ctx, &rpcpb.PutRequest{KeyVal: &kvpb.KV{
+				Key: generateValidKey(b),
+				Val: string(newRandomBytes(256)),
+			}})
+			return err
+		})
+		if err != nil {
+			b.Logf("%v\n", err)
+		}
+	}
+}
+
+func Benchmark_RaftLSM_Service_Get_Linearizable(b *testing.B) {
+	agt := client.NewAgent(context.Background(), "localhost:400", "localhost:401", "localhost:402")
+	defer func(agt client.Agent) { _ = agt.Close() }(agt)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = agt.Pick(func(ctx context.Context, c client.Client) error {
+			_, err := c.Get(ctx, &rpcpb.GetRequest{
+				Key:          generateValidKey(b),
+				Linearizable: true,
+			})
+			return err
+		})
+	}
+}
+
+func Benchmark_RaftLSM_Service_Get_Serializable(b *testing.B) {
+	agt := client.NewAgent(context.Background(), "localhost:400", "localhost:401", "localhost:402")
+	defer func(agt client.Agent) { _ = agt.Close() }(agt)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_ = agt.Pick(func(ctx context.Context, c client.Client) error {
+			_, err := c.Get(ctx, &rpcpb.GetRequest{
+				Key:          generateValidKey(b),
+				Linearizable: false,
+			})
+			return err
+		})
+	}
+}
+
+func Benchmark_Etcd_Service_Put(b *testing.B) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2370", "localhost:2371", "localhost:2372"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		b.Error(err)
+	}
+	ctx := context.Background()
+	defer func(cli *clientv3.Client) { _ = cli.Close() }(cli)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, err := cli.Put(ctx, generateValidKey(b), string(newRandomBytes(256)))
+		if err != nil {
+			b.Logf("%v\n", err)
+		}
+	}
+}
+
+func Benchmark_Etcd_Service_Get_Linearizable(b *testing.B) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2370", "localhost:2371", "localhost:2372"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		b.Error(err)
+	}
+	ctx := context.Background()
+	defer func(cli *clientv3.Client) { _ = cli.Close() }(cli)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = cli.Get(ctx, generateValidKey(b))
+	}
+}
+
+func Benchmark_Etcd_Service_Get_Serializable(b *testing.B) {
+	cli, err := clientv3.New(clientv3.Config{
+		Endpoints:   []string{"localhost:2370", "localhost:2371", "localhost:2372"},
+		DialTimeout: 5 * time.Second,
+	})
+	if err != nil {
+		b.Error(err)
+	}
+	ctx := context.Background()
+	defer func(cli *clientv3.Client) { _ = cli.Close() }(cli)
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		_, _ = cli.Get(ctx, generateValidKey(b), clientv3.WithSerializable())
+	}
+}
+
+func generateValidKey(b *testing.B) string {
+	var key string
+	b.StopTimer()
+	defer b.StartTimer()
+GEN:
+	key = string(newRandomBytes(8))
+	if !backend.ValidateKey(key) {
+		goto GEN
+	}
+	return key
+}
--- go.mod
+++ go.mod
@@ -9,7 +9,9 @@ require (
 	github.com/json-iterator/go v1.1.11
 	github.com/spf13/cobra v1.1.3
 	github.com/syndtr/goleveldb v1.0.0
+	go.etcd.io/bbolt v1.3.6
 	go.etcd.io/etcd/client/pkg/v3 v3.5.2
+	go.etcd.io/etcd/client/v3 v3.5.2
 	go.etcd.io/etcd/raft/v3 v3.5.2
 	go.etcd.io/etcd/server/v3 v3.5.2
 	go.uber.org/zap v1.17.0
@@ -46,7 +48,6 @@ require (
 	github.com/prometheus/procfs v0.6.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
-	go.etcd.io/bbolt v1.3.6 // indirect
 	go.etcd.io/etcd/api/v3 v3.5.2 // indirect
 	go.etcd.io/etcd/client/v2 v2.305.2 // indirect
 	go.etcd.io/etcd/pkg/v3 v3.5.2 // indirect
