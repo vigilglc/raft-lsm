93bdf121e55cfb74fd202367cc7cdfb39148dd66
gaolc18 <gaolc18@fudan.edu.cn>
2022-05-14 01:53:50 +0800

    Add function to create and manipulate multiple benchmark clients.

--- benchmark/service_concurent_test.go
+++ benchmark/service_concurent_test.go
@@ -2,34 +2,40 @@ package benchmark
 
 import (
 	"context"
+	"fmt"
 	"github.com/vigilglc/raft-lsm/client"
 	"github.com/vigilglc/raft-lsm/server/api/rpcpb"
 	"github.com/vigilglc/raft-lsm/server/backend/kvpb"
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"sync/atomic"
 	"testing"
 	"time"
 )
 
 func Benchmark_Concurrent1_RaftLSM_Service_Put(b *testing.B) {
-	benchmarkConcurrentRaftLSMServicePutCommon(b, 1)
+	benchmarkConcurrentRaftLSMServicePutCommon(b, 1, 1)
 }
 func Benchmark_Concurrent2_RaftLSM_Service_Put(b *testing.B) {
-	benchmarkConcurrentRaftLSMServicePutCommon(b, 2)
+	benchmarkConcurrentRaftLSMServicePutCommon(b, 2, 1)
 }
 func Benchmark_Concurrent4_RaftLSM_Service_Put(b *testing.B) {
-	benchmarkConcurrentRaftLSMServicePutCommon(b, 4)
+	benchmarkConcurrentRaftLSMServicePutCommon(b, 4, 1)
 
 }
 func Benchmark_Concurrent8_RaftLSM_Service_Put(b *testing.B) {
-	benchmarkConcurrentRaftLSMServicePutCommon(b, 8)
+	benchmarkConcurrentRaftLSMServicePutCommon(b, 8, 1)
 }
-func benchmarkConcurrentRaftLSMServicePutCommon(b *testing.B, parallelism int) {
-	agt := client.NewAgent(context.Background(), "localhost:400", "localhost:401", "localhost:402")
-	defer func(agt client.Agent) { _ = agt.Close() }(agt)
+func benchmarkConcurrentRaftLSMServicePutCommon(b *testing.B, parallelism int, clientCount int) {
+	fetchAgent, release, err := createRaftLSMClients(clientCount)
+	if err != nil {
+		b.Error(err)
+	}
+	defer release()
 	b.SetParallelism(parallelism)
 	b.ResetTimer()
 	defer b.StopTimer()
 	b.RunParallel(func(pb *testing.PB) {
+		agt := fetchAgent()
 		for pb.Next() {
 			err := agt.Pick(func(ctx context.Context, c client.Client) error {
 				_, err := c.Put(ctx, &rpcpb.PutRequest{KeyVal: &kvpb.KV{
@@ -58,12 +64,16 @@ func Benchmark_Concurrent8_Linearizable_RaftLSM_Service_Get(b *testing.B) {
 	benchmarkConcurrentLinearizableRaftLSMServiceGetCommon(b, 8)
 }
 func benchmarkConcurrentLinearizableRaftLSMServiceGetCommon(b *testing.B, parallelism int) {
-	agt := client.NewAgent(context.Background(), "localhost:400", "localhost:401", "localhost:402")
-	defer func(agt client.Agent) { _ = agt.Close() }(agt)
+	fetchAgent, release, err := createRaftLSMClients(1)
+	if err != nil {
+		b.Error(err)
+	}
+	defer release()
 	b.SetParallelism(parallelism)
 	b.ResetTimer()
 	defer b.StopTimer()
 	b.RunParallel(func(pb *testing.PB) {
+		agt := fetchAgent()
 		for pb.Next() {
 			_ = agt.Pick(func(ctx context.Context, c client.Client) error {
 				_, err := c.Get(ctx, &rpcpb.GetRequest{
@@ -89,12 +99,16 @@ func Benchmark_Concurrent8_Serializable_RaftLSM_Service_Get(b *testing.B) {
 	benchmarkConcurrentSerializableRaftLSMServiceGetCommon(b, 8)
 }
 func benchmarkConcurrentSerializableRaftLSMServiceGetCommon(b *testing.B, parallelism int) {
-	agt := client.NewAgent(context.Background(), "localhost:400", "localhost:401", "localhost:402")
-	defer func(agt client.Agent) { _ = agt.Close() }(agt)
+	fetchAgent, release, err := createRaftLSMClients(1)
+	if err != nil {
+		b.Error(err)
+	}
+	defer release()
 	b.SetParallelism(parallelism)
 	b.ResetTimer()
 	defer b.StopTimer()
 	b.RunParallel(func(pb *testing.PB) {
+		agt := fetchAgent()
 		for pb.Next() {
 			_ = agt.Pick(func(ctx context.Context, c client.Client) error {
 				_, err := c.Get(ctx, &rpcpb.GetRequest{
@@ -108,31 +122,29 @@ func benchmarkConcurrentSerializableRaftLSMServiceGetCommon(b *testing.B, parall
 }
 
 func Benchmark_Concurrent1_Etcd_Service_Put(b *testing.B) {
-	benchmarkConcurrentEtcdServicePutCommon(b, 1)
+	benchmarkConcurrentEtcdServicePutCommon(b, 1, 1)
 }
 func Benchmark_Concurrent2_Etcd_Service_Put(b *testing.B) {
-	benchmarkConcurrentEtcdServicePutCommon(b, 2)
+	benchmarkConcurrentEtcdServicePutCommon(b, 2, 1)
 }
 func Benchmark_Concurrent4_Etcd_Service_Put(b *testing.B) {
-	benchmarkConcurrentEtcdServicePutCommon(b, 4)
+	benchmarkConcurrentEtcdServicePutCommon(b, 4, 1)
 }
 func Benchmark_Concurrent8_Etcd_Service_Put(b *testing.B) {
-	benchmarkConcurrentEtcdServicePutCommon(b, 8)
+	benchmarkConcurrentEtcdServicePutCommon(b, 8, 1)
 }
-func benchmarkConcurrentEtcdServicePutCommon(b *testing.B, parallelism int) {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"localhost:2370", "localhost:2371", "localhost:2372"},
-		DialTimeout: 5 * time.Second,
-	})
+func benchmarkConcurrentEtcdServicePutCommon(b *testing.B, parallelism int, clientCount int) {
+	fetchClient, release, err := createEtcdClients(clientCount)
 	if err != nil {
 		b.Error(err)
 	}
+	defer release()
 	ctx := context.Background()
-	defer func(cli *clientv3.Client) { _ = cli.Close() }(cli)
 	b.SetParallelism(parallelism)
 	b.ResetTimer()
 	defer b.StopTimer()
 	b.RunParallel(func(pb *testing.PB) {
+		cli := fetchClient()
 		for pb.Next() {
 			_, err := cli.Put(ctx, generateValidKey(b), string(newRandomBytes(256)))
 			if err != nil {
@@ -155,19 +167,17 @@ func Benchmark_Concurrent8_Linearizable_Etcd_Service_Get(b *testing.B) {
 	benchmarkConcurrentLinearizableEtcdServiceGetCommon(b, 8)
 }
 func benchmarkConcurrentLinearizableEtcdServiceGetCommon(b *testing.B, parallelism int) {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"localhost:2370", "localhost:2371", "localhost:2372"},
-		DialTimeout: 5 * time.Second,
-	})
+	fetchClient, release, err := createEtcdClients(1)
 	if err != nil {
 		b.Error(err)
 	}
+	defer release()
 	ctx := context.Background()
-	defer func(cli *clientv3.Client) { _ = cli.Close() }(cli)
 	b.SetParallelism(parallelism)
 	b.ResetTimer()
 	defer b.StopTimer()
 	b.RunParallel(func(pb *testing.PB) {
+		cli := fetchClient()
 		for pb.Next() {
 			_, _ = cli.Get(ctx, generateValidKey(b))
 		}
@@ -187,21 +197,72 @@ func Benchmark_Concurrent8_Serializable_Etcd_Service_Get(b *testing.B) {
 	benchmarkConcurrentSerializableEtcdServiceGetCommon(b, 8)
 }
 func benchmarkConcurrentSerializableEtcdServiceGetCommon(b *testing.B, parallelism int) {
-	cli, err := clientv3.New(clientv3.Config{
-		Endpoints:   []string{"localhost:2370", "localhost:2371", "localhost:2372"},
-		DialTimeout: 5 * time.Second,
-	})
+	fetchClient, release, err := createEtcdClients(1)
 	if err != nil {
 		b.Error(err)
 	}
+	defer release()
 	ctx := context.Background()
-	defer func(cli *clientv3.Client) { _ = cli.Close() }(cli)
 	b.SetParallelism(parallelism)
 	b.ResetTimer()
 	defer b.StopTimer()
 	b.RunParallel(func(pb *testing.PB) {
+		cli := fetchClient()
 		for pb.Next() {
 			_, _ = cli.Get(ctx, generateValidKey(b), clientv3.WithSerializable())
 		}
 	})
+}
+
+func createRaftLSMClients(count int) (fetchClient func() client.Agent, release func(), err error) {
+	if count <= 0 {
+		err = fmt.Errorf("no clients created")
+		return
+	}
+	var agents []client.Agent
+	for i := 0; i < count; i++ {
+		agt := client.NewAgent(context.Background(), "localhost:400", "localhost:401", "localhost:402")
+		agents = append(agents, agt)
+	}
+	var agtIdx uint64
+	fetchClient = func() client.Agent {
+		return agents[(atomic.AddUint64(&agtIdx, 1)-1)%uint64(count)]
+	}
+	release = func() {
+		for i := 0; i < count; i++ {
+			_ = agents[i].Close()
+		}
+	}
+	return
+}
+
+func createEtcdClients(count int) (fetchClient func() *clientv3.Client, release func(), err error) {
+	if count <= 0 {
+		err = fmt.Errorf("no clients created")
+		return
+	}
+	var clients []*clientv3.Client
+	for i := 0; i < count; i++ {
+		cli, err := clientv3.New(clientv3.Config{
+			Endpoints:   []string{"localhost:2370", "localhost:2371", "localhost:2372"},
+			DialTimeout: 5 * time.Second,
+		})
+		if err != nil {
+			for j := 0; j < i; j++ {
+				_ = clients[j].Close()
+			}
+			return nil, nil, err
+		}
+		clients = append(clients, cli)
+	}
+	var agtIdx uint64
+	fetchClient = func() *clientv3.Client {
+		return clients[(atomic.AddUint64(&agtIdx, 1)-1)%uint64(count)]
+	}
+	release = func() {
+		for i := 0; i < count; i++ {
+			_ = clients[i].Close()
+		}
+	}
+	return
 }
